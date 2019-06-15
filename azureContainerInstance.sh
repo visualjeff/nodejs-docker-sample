a1a52097566d3b8a8c05692088a8d1833ef71ad7
visualjeff <visualjeff@icloud.com>
2019-06-15 10:01:53 -0700

    azure script edit

--- azureContainerInstance.sh
+++ azureContainerInstance.sh
@@ -1,18 +1,18 @@
 # Script to create an Azure Container Instance.  This assumes you already have a resource group established on Azure.
 
-DNS_NAME_LABEL=test-nodejs-app-$RANDOM
+DNS_NAME_LABEL=nodejs-docker-sample-$RANDOM
 
 az container create \
   --resource-group << RESOURCE-GROUP >> \
-  --name test-nodejs-app \
-  --image visualjeff/test-nodejs-app:latest \
+  --name nodejs-docker-sample \
+  --image visualjeff/nodejs-docker-sample:latest \
   --ports 1337 \
   --dns-name-label $DNS_NAME_LABEL \
   --location westus2
 
 az container show \
   --resource-group << RESOURCE-GROUP  >> \
-  --name test-nodejs-app \
+  --name nodejs-docker-sample \
   --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" \
   --out table
 
